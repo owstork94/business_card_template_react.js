626cbef41ca6ce6a03c5cc55ddd03d07c59e4bf9
owstork94 <70260553+owstork94@users.noreply.github.com>
2021-07-19 00:10:40 +0900

    css를 이용하여 로딩스피너 구현

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,15 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css'
 
 const Imgefileinput = ({ ImageUploader, name, onFileChange }) => {
+    const [loading, setloading] = useState(false);
     const inputRef = useRef();
     const onButtonclick = (event) => {
         event.preventDefault();
         inputRef.current.click();
     }
     const onChange = async event => {
+        setloading(true);
         console.log(event.target.files[0]);
         const uploaded = await ImageUploader.upload(event.target.files[0]);
+        setloading(false);
         console.log(uploaded)
         onFileChange({
             name: uploaded.original_filename,
@@ -26,8 +29,14 @@ const Imgefileinput = ({ ImageUploader, name, onFileChange }) => {
                 name="file"
                 onChange={onChange}
             />
-            <button className={styles.button} onClick={onButtonclick}>{name || 'No file'}</button>
-        </div>
+            {!loading && (
+                <button button className={`${styles.button} ${name ? styles.pink : styles.grey}`} //중요포인트.
+                    onClick={onButtonclick}>
+                    {name || 'No file'}
+                </button>
+            )}
+            {loading && <div className={styles.loading}></div>}
+        </div >
     );
 
 }
